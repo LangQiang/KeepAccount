45d1378be47d678525e30e27e2f1bcb1afa800a7
强 <lq910518@126.com>
2024-04-16 00:07:12 +0800

    对齐账面

--- user-portal/src/main/java/com/godq/portal/constants/DataConstants.kt
+++ user-portal/src/main/java/com/godq/portal/constants/DataConstants.kt
@@ -5,5 +5,5 @@ package com.godq.portal.constants
  * @author  GodQ
  * @date  2023/11/12 22:42
  */
-//  初始剩余 - 加盟费 - 工程欠款
-const val BILL_TOTAL_DIFF: Float = 95008.47f - 100000f - 100000f
+//  初始剩余 - 加盟费 - 工程欠款 - 账面对齐差额
+const val BILL_TOTAL_DIFF: Float = 95008.47f - 100000f - 100000f - 3054.45f
